42f6cc2afb406d7e081f9af6de69a775e941796a
Pavel Chaika <mrpavel.chaika@gmail.com>
2023-12-09 10:00:39 +0300

    Fix unusable var

--- tests/test_homework6.py
+++ tests/test_homework6.py
@@ -25,7 +25,6 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
     if dark_theme_enabled_by_user:
         is_dark_theme = True
     elif dark_theme_enabled_by_user is None and 22 <= current_time.hour:
